15cb8c60c2db33be1007e9ddb4425aa39e637872
Igor Sprovieri <igorsprovieri@MacBook-Air-de-Igor.local>
2023-01-04 19:09:23 -0300

    setting: mongoDB database

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const port = 3000;
+const port = 3333;
+
+async function connectDataBase() {
+  await mongoose.connect("mongodb://localhost:27017");
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
+  mongoose.set("strictQuery", true);
+  connectDataBase().catch((error) => {
+    console.log(error);
+  });
   console.log(`Example app listening on port ${port}`);
 });
