03d11338a8d3e1eaf3c3b57087f247e1463274d2
Igor Sprovieri <igorsprovieri@MacBook-Air-de-Igor.local>
2023-02-24 10:51:40 -0300

    Feat: Cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import { db } from "./db";
 import routes from "./routes";
+import cors from "cors";
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use("/", routes);
 
 app.get("/", (req, res) => {
